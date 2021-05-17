3538887cf9ed94df74f7c2c9da6bfdf0cfe87964
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-05-17 22:56:42 +0900

    seeds.rbにデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |n| #　レストランは表示上三つだけなので3.times
+  restaurant = Restaurant.new( # 三回Restaurant.new()させる
+    name: "testレストラン_#{n}",
+    fee: 100,
+    time_required: 10,
+  )
+
+  12.times do |m| # Restaurantにつき12このFoodも作成
+    restaurant.foods.build( # restaurant.foods.buildとすることで、Food.newすることなくリレーションを持ったfoodを生成することができる
+      name: "フード名_#{m}",
+      price: 500,
+      description: "フード_#{m}の説明文です。"
+    )
+  end
+
+  restaurant.save! # 最後に生成したrestaurantインスタンスsave!とすることでデータをDBに書き込むことができます。
+end
