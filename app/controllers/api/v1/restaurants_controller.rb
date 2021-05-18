0a277f2cfb27257c0fea8e08dab0d336b6fea316
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-05-18 10:41:46 +0900

    foods_controllerの作成

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,12 +1,12 @@
-module Api
+module Api # 名前空間を指定している
   module V1
     class RestaurantsController < ApplicationController
       def index
-        restaurants = Restaurant.all
+        restaurants = Restaurant.all # Restaurantモデルを全て習得
 
-        render json: {
+        render json: { #JSON形式でデータを返している
           restaurants: restaurants
-        }, status: :ok
+        }, status: :ok # このようにすることでリクエストが成功したこと、200OKと一緒にデータを返す
       end
     end
   end
