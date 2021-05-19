a5484be298e8215dd9514007bc3d9ae1b6c512bb
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-05-19 14:53:16 +0900

    Create restaurant.js

---
+++ frontend/src/apis/restaurants.js
@@ -0,0 +1,14 @@
+//レストラン一覧のAPIを呼ぶ関数だけのがファイル
+
+import axios from 'axios'
+import { restaurantIndex } from '../urls/index'
+
+export const fetchRestaurants = () => {
+  return axios.get(restaurantIndex) //今回はgetですが、POSTであればaxios.post()のようになります。
+  .then(res => { //成功した場合の処理 resという名前で取得し、res.dataでレスポンスの中身だけをreturnしています
+    return res.data
+  })
+  .catch((e) => console.error(e)) //失敗・例外 コンソールでエラーを出すだけにしています。
+}
+
+//axiosはPromiseベースであること。つまりaxiosを使う側でnew Promiseなどしなくても非同期処理を実装することができる
