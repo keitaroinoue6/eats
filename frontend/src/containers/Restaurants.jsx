980c8769421539b90e56647936a85081d3d010b3
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-05-19 21:19:10 +0900

    Update Restaurants.jsx

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,33 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components'; //styledコンポーネントが使えるようになる
 //apis
 import { fetchRestaurants } from "../apis/restaurants";
+
+//images
+import MainLogo from '../images/logo.png' //画像名前をimportする
+import MainCoverImage from '../images/main-cover-image.png'
+
+//ページの一番上部にあるヘッダー全体を定義
+const HeaderWrapper = styled.div` 
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+//ヘッダーのロゴ
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+//トップページ真ん中のメイン画像
+const MainCover = styled.img`
+  height: 600px;
+`;
+
 
 export const Restaurants = () => {
   useEffect(() => {
@@ -11,7 +38,12 @@ export const Restaurants = () => {
   }, []) //今回はコンポーネントのレンダリングを一回だけにしたいため第二引数に空配列をいれる
   return (
     <>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo"></MainLogoImage>
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover"></MainCover>
+      </MainCoverImageWrapper>
     </>
   )
 }
