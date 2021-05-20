6d6f739367cfa31b2caebac20d6323a3b00cff4c
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-05-20 10:30:11 +0900

    Restaurants.jsxにReducerの導入

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import styled from 'styled-components'; //styledコンポーネントが使えるようになる
 //apis
 import { fetchRestaurants } from "../apis/restaurants";
+
+
+//reducers
+import { initialState, restaurantsActionTypes, restaurantsReducer } from '../reducers/restaurants';
 
 //images
 import MainLogo from '../images/logo.png' //画像名前をimportする
@@ -30,20 +34,34 @@ const MainCover = styled.img`
 
 
 export const Restaurants = () => {
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
   useEffect(() => {
+    dispatch({type: restaurantsActionTypes.FETCHING})
     fetchRestaurants() //引数を取らずにPromiseを返す。成功の場合にはres.dataを返す。その結果を(data)の形にする
     .then((data) =>
-      console.log(data)
+      dispatch({
+        type: restaurantsActionTypes.FETCH_SUCCESS,
+        payload: {
+          restaurants: data.restaurants
+        }
+      })
     )
   }, []) //今回はコンポーネントのレンダリングを一回だけにしたいため第二引数に空配列をいれる
   return (
     <>
       <HeaderWrapper>
-        <MainLogoImage src={MainLogo} alt="main logo"></MainLogoImage>
+        <MainLogoImage src={MainLogo} alt="main logo" />
       </HeaderWrapper>
       <MainCoverImageWrapper>
-        <MainCover src={MainCoverImage} alt="main cover"></MainCover>
+        <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
+      {
+        state.restaurantsList.map(restaurant =>
+          <div>
+            {restaurant.name}
+          </div>
+        )
+      }
     </>
   )
 }
--- frontend/src/reducers/restaurants.js
+++ frontend/src/reducers/restaurants.js
@@ -3,7 +3,7 @@ import { REQUEST_STATE } from '../constants';
 
 export const initialState = { //initialStateは初期値stateで、useReducerに渡す
   fetchState: REQUEST_STATE.INITIAL, //GET APIの状態を表す。一般的にAPIからデータを取得するときにfetch...という名前にする
-  restaurantList: [], //APIカラ取得したレストラン一覧が入ってきます。初期値は空配列として[]を入れておく
+  restaurantsList: [], //APIカラ取得したレストラン一覧が入ってきます。初期値は空配列として[]を入れておく
 };
 
 export const restaurantsActionTypes = {
@@ -12,11 +12,11 @@ export const restaurantsActionTypes = {
 }
 
 export const restaurantsReducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case restaurantsActionTypes.FETCHING: //API取得
       return {
         ...state,
-        ferchState: REQUEST_STATE.LOADING, //API取得中→fetchStateはLOADINGにスイッチする
+        fetchState: REQUEST_STATE.LOADING, //API取得中→fetchStateはLOADINGにスイッチする
       };
       case restaurantsActionTypes.FETCH_SUCCESS:
         return {
