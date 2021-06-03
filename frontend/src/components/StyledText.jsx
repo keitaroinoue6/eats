85d24170f49f5276decade1b816776441ac62d1a
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-06-03 23:16:49 +0900

    commit

--- frontend/src/components/StyledText.jsx
+++ frontend/src/components/StyledText.jsx
@@ -1,2 +1,8 @@
 import styled from 'styled-components';
 import { COLORS, FONT_SIZE } from '../style_constants';
+
+//SubTextを定義しているだけ。これをimportして使うことでサブテキストサブテキスト
+export const SubText = styled.p`
+  color: ${COLORS.SUB_TEXT};
+  font-size: ${FONT_SIZE.BODY2};
+`;
