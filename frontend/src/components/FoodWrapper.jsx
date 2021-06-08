1bd1716ff082f3850a0aad0414b3c8a264a41a37
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-06-08 22:03:13 +0900

    commit

--- frontend/src/components/FoodWrapper.jsx
+++ frontend/src/components/FoodWrapper.jsx
@@ -6,3 +6,14 @@ import { SubText } from './StyledText';
 
 //constants
 import { COLORS } from '..//style_constants';
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 450px;
+    height: 180px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: ${COLORS.BORDER};
+    border-image: initial;
+    cursor: pointer;
+`;
